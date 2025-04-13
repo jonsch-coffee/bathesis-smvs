b1329920b7815ff4913f245048fc3ed087c10c7f
Jonathan <bring@jonsch.coffee>
2025-04-13 18:13:57 +0200

    added keyboard shortcuts

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,30 @@
 <script setup>
 // eslint-disable-next-line
 import bootstrap from 'bootstrap'
+
+import { onMounted, onBeforeUnmount } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function handleKeyboardShortcut(e) {
+  if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === 'e') {
+    e.preventDefault()
+    router.push('/editor')
+  }
+  if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === 'x') {
+    e.preventDefault()
+    router.push('/')
+  }
+}
+
+onMounted(() => {
+  window.addEventListener('keydown', handleKeyboardShortcut)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('keydown', handleKeyboardShortcut)
+})
 </script>
 
 <template>
