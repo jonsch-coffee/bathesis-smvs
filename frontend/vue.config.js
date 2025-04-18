9e0b721482d5e11cc4783db74f278aa874011beb
Jonathan <67755045+jonsch-coffee@users.noreply.github.com>
2025-04-18 19:36:15 +0200

    Debugging: frontend/dist

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/',
-  indexPath: '/index.html',
+  publicPath: '/frontend/dist/',
   transpileDependencies: true
 })
