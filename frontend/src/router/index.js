695dec96e1b187a18a9b57c6645c9f577327dfc1
Jonathan <67755045+jonsch-coffee@users.noreply.github.com>
2025-04-18 19:58:57 +0200

    Debugging: 404 not found

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,12 +3,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import GuideEditorView from "@/views/GuideEditorView.vue";
 import HomeView from "@/views/HomeView.vue";
 import SimpleView from "@/views/SimpleView.vue";
+import NotFound from "@/views/NotFound.vue";
 
 
 const routes = [
     { path: '/', name: 'home', component: HomeView },
     { path: '/editor', name: 'editor', component: GuideEditorView },
     { path: '/test', name: 'test', component: SimpleView },
+    { path: '/:pathMatch(.*)', component: NotFound }
 ]
 
 const router = createRouter({
