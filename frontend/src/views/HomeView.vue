6ba30d6a8b947a8dc706a6ea7ef58fbf0af1013c
Jonathan <67755045+jonsch-coffee@users.noreply.github.com>
2025-04-18 19:00:52 +0200

    Debugging

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,10 +3,14 @@
 <script setup>
 /* eslint-disable */
 import TypeAheadComponent from '@/components/TypeAheadComponent.vue'
+import { onMounted, onBeforeUnmount } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 </script>
 
 <template>
   <div class="d-flex flex-column min-vh-100 justify-content-center align-items-center">
-    <TypeAheadComponent />
+    <button @click="router.push('/editor')">Editor öffnen</button>
   </div>
 </template>
