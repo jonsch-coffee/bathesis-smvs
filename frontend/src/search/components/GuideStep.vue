bf5a413cec9c5e8441b31fb3f23da9492b651271
Jonathan <67755045+jonsch-coffee@users.noreply.github.com>
2025-05-11 20:02:44 +0200

    Markdown

--- frontend/src/search/components/GuideStep.vue
+++ frontend/src/search/components/GuideStep.vue
@@ -1,15 +1,21 @@
 <script setup>
+import MarkdownIt from 'markdown-it'
 // eslint-disable-next-line
-defineProps({
+const markdown = new MarkdownIt()
+// eslint-disable-next-line
+const props = defineProps({
   questionTitle: String,
-  description: String,
+  description: String
 })
+
+const renderedDescription = markdown.render(props.description)
+
 </script>
 
 <template>
   <div>
     <p class="lead mt-3">{{ questionTitle }}</p>
     <hr class="my-4">
-    <p>{{ description }}</p>
+    <div v-html="renderedDescription" />
   </div>
 </template>
